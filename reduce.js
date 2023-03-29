0a7c3a625a210949abf5a68f93c64254832098bb
andrewcannan <andrewmcannan@gmail.com>
2023-03-29 22:27:26 +0000

    initial commit

--- reduce.js
+++ reduce.js
@@ -7,6 +7,16 @@
 
 // Summing an array of numbers:
 
+const nums = [0, 1, 2, 3, 4];
+let sum = nums.reduce((acc, curr) => {
+  console.log(
+  'accumulator:', acc,
+  'current', curr,
+  'total:', acc + curr,);
+
+  return acc + curr
+});
+console.log(sum);
 
 const teamMembers = [
   {
@@ -33,5 +43,56 @@ const teamMembers = [
 
 // Totaling a specific object property
 
+const totalExperience = teamMembers.reduce((acc, curr) => acc + curr.yrsExperience, 0);
+console.log(totalExperience);
 
 // Grouping by a property, and totaling it too
+// {Developer: 12, Designer: 4} ---- this is what we want!
+const experienceByProfession = teamMembers.reduce((acc, curr) => {
+  let key = curr.profession;
+  if (!acc[key]) {
+    acc[key] = curr.yrsExperience; //check if key (profession) is not already in new object and set curr to its value
+  } else {
+    acc[key] += curr.yrsExperience; //if key (profession) already exists add value to curr
+  }
+  return acc;
+}, {});
+
+console.log(experienceByProfession);
+
+// CI challenge answer
+
+let students = [
+  {
+      name: 'John',
+      subjects: ['maths', 'english', 'cad'],
+      teacher: {maths: 'Harry', english: 'Joan', cad: 'Paul'},
+      results: {maths: 90, english: 75, cad: 87},
+  },
+  {
+      name: 'Emily',
+      subjects: ['science', 'english', 'art'],
+      teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+      results: {science: 93, english: 73, art: 95},
+  },
+  {
+      name: 'Adam',
+      subjects: ['science', 'maths', 'art'],
+      teacher: {science: 'Iris', maths: 'Harry', art: 'Simon'},
+      results: {science: 93, english: 88, maths: 97, art: 95},
+  },
+  {
+      name: 'Fran',
+      subjects: ['science', 'english', 'art'],
+      teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+      results: {science: 93, english: 87, art: 95},
+  }
+];
+
+const biggest = students.reduce((acc, curr) => {  //acc = total, curr = current position in array
+  if (curr.results.english > acc.max ){  // if the current result.english is greater than 'max' value in acc at end
+      return {name: curr.name, max: curr.results.english}; //return this object
+  }
+  return acc;
+}, {name:'', max: 0});  // object here is the acc
+console.log(biggest)
